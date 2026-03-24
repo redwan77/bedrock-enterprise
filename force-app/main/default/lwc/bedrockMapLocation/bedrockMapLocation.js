bfd9bc6fdddb099a1cd626b15d6d1d2e86b3fdd5
relhammouti <relhammouti@gmail.com>
2026-03-24 04:57:53 +0100

    feat(metadata): add BedRockLocationMetadataController and custom metadata for headquarters location

--- force-app/main/default/lwc/bedrockMapLocation/bedrockMapLocation.js
+++ force-app/main/default/lwc/bedrockMapLocation/bedrockMapLocation.js
@@ -1,24 +1,49 @@
 // bedrockMapLocation.js
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getHeadQuarterLocation from '@salesforce/apex/BedRockLocationMetadataController.getHeadQuarterLocation';
 
 const DEFAULT_ZOOM = 12;
+const MARKER_TITLE = 'Bedrock Headquarters';
+const MARKER_DESCRIPTION = 'Bedrock Corporate Headquarters';
 
 export default class BedrockMapLocation extends LightningElement {
     zoomLevel = DEFAULT_ZOOM;
 
-    mapMarkers = [
-        {
-            location: {
-                Street: 'The Landmark @ One Market, Suite 300',
-                City: 'San Francisco',
-                State: 'CA',
-                PostalCode: '94105',
-                Country: 'US'
-            },
-            title: 'Bedrock Headquarters',
-            description: 'San Francisco, CA 94105',
-            icon: 'standard:account',
-            value: 'BEDROCK_HQ'
+    @wire(getHeadQuarterLocation)
+    headquartersLocation;
+
+    get hasLocation() {
+        return !!this.headquartersLocation?.data;
+    }
+
+    get hasError() {
+        return !!this.headquartersLocation?.error;
+    }
+
+    get isLoading() {
+        return !this.hasLocation && !this.hasError;
+    }
+
+    get mapMarkers() {
+        const locationData = this.headquartersLocation?.data;
+        if (!locationData) {
+            return [];
         }
-    ];
+
+        return [
+            {
+                location: {
+                    Street: locationData.Street__c,
+                    City: locationData.City__c,
+                    State: locationData.State__c,
+                    PostalCode: locationData.PostalCode__c,
+                    Country: locationData.Country__c
+                },
+                title: MARKER_TITLE,
+                description: MARKER_DESCRIPTION,
+                icon: 'standard:account',
+                value: 'BEDROCK_HQ'
+            }
+        ];
+    }
 }
